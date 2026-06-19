871281ee01ff936b1fc126a77a16919c68846d9e
TalhaBinShakoor <talhabinshakoor100@gmail.com>
2026-06-19 18:51:28 +0200

    feat: add Angular layout and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard').then(
+        (component) => component.Dashboard,
+      ),
+  },
+  {
+  path: '**',
+  loadComponent: () =>
+    import('./features/not-found/not-found').then(
+      (component) => component.NotFound,
+    ),
+},
+];
